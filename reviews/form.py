3b20edb1980183682d8e618994ce0f6136253967
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2022-08-31 18:00:29 +0100

    Configure Modelform

--- reviews/form.py
+++ reviews/form.py
@@ -1,12 +1,34 @@
 from django import forms
 
 
-class ReviewForm(forms.Form):
-    username = forms.CharField(label='Your Username',
-                               max_length=50,
-                               error_messages={
-                                   'required': 'This field must not be empty!',
-                                   'max_length': 'Please enter a shorter name!'
-                               })
-    review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=250)
-    rating = forms.IntegerField(label='Your Rating', max_value=5, min_value=1)
+# class ReviewForm(forms.Form):
+#     username = forms.CharField(label='Your Username',
+#                                max_length=50,
+#                                error_messages={
+#                                    'required': 'This field must not be empty!',
+#                                    'max_length': 'Please enter a shorter name!'
+#                                })
+#     review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=250)
+#     rating = forms.IntegerField(label='Your Rating', max_value=5, min_value=1)
+from reviews.models import Review
+
+
+# Creates a form using the model
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        # points to the model to get the fields from
+        model = Review
+        # to include all the fields
+        fields = '__all__'
+        # exclude = ['owner_comment']
+        labels = {
+            'username': 'Your Username',
+            'review_text': 'Your Feedback',
+            'rating': 'Your Rating'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty!',
+                'max_length': 'Please enter a shorter name!'
+            }
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -2,26 +2,36 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
 from reviews.form import ReviewForm
-from reviews.models import Review
 
 
-def review(request):
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
+
+    def post(self, request):
+        # To edit an existing entry
+        # from reviews.models import Review
+        # data_to_delete = Review.objects.get(pk=1)
+        # form = ReviewForm(request.POST, instance=data_to_delete)
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = Review(
-                username=form.cleaned_data['username'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating']
-            )
-            review.save()
+            # review = Review(
+            #     username=form.cleaned_data['username'],
+            #     review_text=form.cleaned_data['review_text'],
+            #     rating=form.cleaned_data['rating']
+            # )
+            # review.save()
+            form.save()
             return HttpResponseRedirect('/thank-you')
-    else:
-        form = ReviewForm()
-    return render(request, 'reviews/review.html', {
-        'form': form
-    })
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
 
 
 def thank_you(request):
